447d176fe97c688f4d8a34608e68d273868c0396
Rinzler84 <81184471+Rinzler84@users.noreply.github.com>
2021-04-20 12:38:32 -0400

    Add files via upload
    Alternative facial recognition code

---
+++ python/RecognizeUser.py
@@ -0,0 +1,45 @@
+# This is the first attempt at getting facial recognition working.
+
+# Import necessary modules
+import face_recognition 
+import picamera
+import numpy as np
+
+# Create a reference to our camera
+camera = picamera.PiCamera()
+camera.resolution = (352, 240)
+output = np.empty((240, 352, 3), dtype=np.uint8) # Creates an array  of given dimensions
+
+# Load the picture of the user and learn to recognize it
+user_image = face_recognition.load_image_file("alex1.png")
+user_face_encoding = face_recognition.face_encodings(user_image)[0]
+
+# Initialize 2 lists for storing face details and encodings
+face_locations = []
+face_encodings = []
+
+# Get a picture of the user currently in front of mirror
+camera.capture(output, format = "rgb")
+
+# Find the face and face encoding in the current frame
+face_locations = face_recognition.face_locations(output)
+
+# If you can't read the face, notify the user
+if(len(face_locations) == 0):
+    print("No Face Detected")
+face_encodings = face_recognition.face_encodings(output, face_locations)
+
+# Check to see if the current person in the frame is the user
+for face_encoding in face_encodings:
+    match = face_recognition.compare_faces([user_face_encoding], face_encoding)
+    name = ""
+
+    # If the user is in the frame, define the users name
+    if match[0]:
+        name = "Alex"
+        print("Access Granted. \nHello {}!".format(name))
+    else:
+        print("Access Denied")
+
+    
+
