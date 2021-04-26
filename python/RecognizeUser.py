5dc2ab3259d9e81b9f8c2c32f0dc78242622e1f1
Lynn Yip <50850731+lsyip@users.noreply.github.com>
2021-04-25 22:02:08 -0400

    working module connections
    Module face-rec is working. When user is identified, the display will read "Hello, [name]", otherwise it will ask for fingerprint validation.

--- python/RecognizeUser.py
+++ python/RecognizeUser.py
@@ -1,92 +1,59 @@
+#!/usr/bin/python3
+
 # Import necessary modules
-import face_recognition # Facial Recognition API
-import picamera    # PiCamera module
+import face_recognition
+import picamera
 import numpy as np
-import hashlib
-from pyfingerprint.pyfingerprint import PyFingerprint     # pyfingerprint API
-import RPi.GPIO as GPIO    # Needed for controlling GPIO Pins
+#from .userSetup import getEncodings
 
-# Create a reference to the camera
+# Create a reference to our camera
 camera = picamera.PiCamera()
-camera.resolution = (352, 240)    # Define the camera resolution
-output = np.empty((240, 352, 3), dtype=np.uint8) # Creates an array  of given dimensions
+camera.resolution = (352, 240)
+output = np.empty((240, 352, 3), dtype=np.uint8)  # Creates an array  of given dimensions
 
-# Load the picture of the user.
-userImage = face_recognition.load_image_file("alex1.png")
-userFaceEncoding = face_recognition.face_encodings(userImage)[0]
+def main():
+    # Load the picture of the user and learn to recognize it
+    # You must use the absolute path or else the program can't find it.
+    user_image = face_recognition.load_image_file("/home/pi/MagicMirror/modules/face-rec-module/python/images/lynn_yip.jpg")
+    userFaceEncoding = face_recognition.face_encodings(user_image)[0]
 
-# Create a reference to the fingerprint scanner
-fingerScanner = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
+    # Initialize 2 lists for storing face details and encodings
+    face_locations = []
+    face_encodings = []
 
-# Specify GPIO PIN numbering mode
-GPIO.setmode(GPIO.BOARD)
+    # Get a picture of the user currently in front of mirror
+    camera.capture(output, format = "rgb")
 
-# Initialize 2 lists for storing face details and encodings
-faceLocations = []
-faceEncodings = []
+    # Find the face and face encoding in the current frame
+    face_locations = face_recognition.face_locations(output)
 
-# Get a picture of the user currently in front of the mirror
-camera.capture(output, format = "rgb")
+    # If you can't read the face, notify the user
+    if len(face_locations) == 0:
+        print("No Face Detected")
+    face_encodings = face_recognition.face_encodings(output, face_locations)
 
-# Define the face and face encoding in the current frame
-faceLocations = face_recognition.face_locations(output)
-
-# If you can't read the face, notify the user
-if(len(faceLocations) == 0):
-    print("No Face Detected")
-
-# Encode the face found in frame
-faceEncodings = face_recognition.face_encodings(output, faceLocations)
-
-# compare the current user face encoding with the user encoding in the database
-for faceEncoding in faceEncodings:
-    match = face_recognition.compare_faces([userFaceEncoding], faceEncoding)
-    name = ""
+    # Variable for unlocking solenoid
+    checkLock = 0
+    match = []
+    
+    # Check to see if the current person in the frame is the user
+    for face_encoding in face_encodings:
+        match = face_recognition.compare_faces([userFaceEncoding], face_encoding)
+        name = ""
 
     """
-    Validate that the user is in the frame and grant access to the compartment
-    If it's not the correct user, validate identity through fingerprint
-    scanner, otherwise, deny access.
+    Validate that the user is in the frame and grand access to the compartment.
+    If it's not the user, validate the identity through the fingerprint scanner,
+    otherwise deny access.
     """
-    if match[0]:
-        print("Access Granted. \nHello Alex!")
-        # TODO Activate Solenoid
+    if match:
+        name = "Lynn"
+        print("Access Granted. \nHello {}!".format(name))
+        checkLock = 1
     else:
         print("Access Denied.\nPlease Validate Fingerprint.")
         
-        # Read the given fingerprint image and store it in the image buffer
-        while (fingerScanner.readImage() == False):
-            pass
-        
-        """
-        Convert the image in the image buffer and store it in the specified
-        char buffer
-        """
-        fingerScanner.convertImage(0x01)
+    camera.close()
 
-        """
-        Search the database for the characteristics in the image buffer and
-        create a reference to the tuple being returned
-        """
-        result = fingerScanner.searchTemplate()
-
-        """
-        Get the value stored in the first index of the template search results
-        which indicates in which position of the database the given image was
-        found. -1,  if the fingerprint is not in the database.
-        """
-        positionNumber = result[0]
-
-        """
-        If invalid fingerprint given, Deny access and exit.
-        Otherwise, grant access and activate solenoid to open
-        secret compartment
-        """
-        if (positionNumber == -1):
-            print('Access Denied.')
-            exit(0)
-        else:
-            print("Access Granted. \nHello Alex!")
-            #TODO Activate solenoid
-    
-    
+if __name__ == '__main__':
+    main()
